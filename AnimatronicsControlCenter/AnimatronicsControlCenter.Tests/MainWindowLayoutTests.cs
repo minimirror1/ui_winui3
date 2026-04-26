0208ea62aa08db6188a3615069f7da5fa5bddd06
shs <minimirror1@naver.com>
2026-04-26 20:54:48 +0900

    fix: center serial indicator in compact sidebar

---
+++ AnimatronicsControlCenter/AnimatronicsControlCenter.Tests/MainWindowLayoutTests.cs
@@ -0,0 +1,48 @@
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimatronicsControlCenter.Tests;
+
+[TestClass]
+public class MainWindowLayoutTests
+{
+    [TestMethod]
+    public void SerialTrafficIndicator_IsCenteredWithinCompactPaneFooter()
+    {
+        XNamespace xaml = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+        XNamespace x = "http://schemas.microsoft.com/winfx/2006/xaml";
+        XDocument page = XDocument.Load(ProjectPath("AnimatronicsControlCenter", "MainWindow.xaml"));
+
+        XElement? footerHost = page
+            .Descendants(xaml + "Grid")
+            .SingleOrDefault(element => (string?)element.Attribute(x + "Name") == "SerialTrafficFooterHost");
+
+        Assert.IsNotNull(footerHost, "Serial traffic footer should have a fixed compact-pane host.");
+        Assert.AreEqual("48", (string?)footerHost.Attribute("Width"));
+        Assert.AreEqual("Left", (string?)footerHost.Attribute("HorizontalAlignment"));
+
+        XElement button = footerHost
+            .Descendants(xaml + "Button")
+            .Single(element => (string?)element.Attribute(x + "Name") == "SerialTrafficButton");
+
+        Assert.AreEqual("Center", (string?)button.Attribute("HorizontalAlignment"));
+    }
+
+    private static string ProjectPath(params string[] segments)
+    {
+        DirectoryInfo? directory = new(AppContext.BaseDirectory);
+        while (directory is not null)
+        {
+            string candidate = Path.Combine(new[] { directory.FullName }.Concat(segments).ToArray());
+            if (File.Exists(candidate))
+            {
+                return candidate;
+            }
+
+            directory = directory.Parent;
+        }
+
+        Assert.Fail($"Could not find project file: {Path.Combine(segments)}");
+        return string.Empty;
+    }
+}
